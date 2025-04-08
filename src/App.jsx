e691fa1682473b7fe68df5351812b850761bdbdc
Latif <latif_1995@live.no>
2025-04-08 13:38:22 +0200

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,3 @@
-import {
-  Film,
-  Search,
-  Star,
-  X,
-  Clock,
-  Calendar,
-  Ticket,
-  BookmarkPlus,
-  BookmarkCheck,
-  List,
-} from "lucide-react";
-
 function App() {
   return <p className="bg-yellow-600">solo movie start</p>;
 }
